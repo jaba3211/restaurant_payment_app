771d42a5fba4fdbd11bf02d0a676288fa2a79310
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-06 03:25:24 +0400

    changed qr code js file and style

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 
-/*QR code reader*/
+// /*QR code reader*/
 
-function onScanSuccess(qrCodeMessage) {
-    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
-}
-function onScanError(errorMessage) {
-  //handle scan error
-}
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 30, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess, onScanError);
+// function onScanSuccess(qrCodeMessage) {
+//     document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+// }
+// function onScanError(errorMessage) {
+//   //handle scan error
+// }
+// var html5QrcodeScanner = new Html5QrcodeScanner(
+//     "reader", { fps: 30, qrbox: 250 });
+// html5QrcodeScanner.render(onScanSuccess, onScanError);
--- resources/views/modules/frontend/QR/qr.blade.php
+++ resources/views/modules/frontend/QR/qr.blade.php
@@ -1,23 +1,27 @@
 @extends('parent')
 @section('content')
-    <div id="reader"></div>
-    <div class="linking text-center mt-5">
-        <a href="http://localhost:8000/categories/2/5" id="link_to_menu" class="btn btn-success">გადადით ლინკზე</a>
-    </div>
-    <script src="/html5-qrcode.min.js"></script>
-    <script>
-        var linking = document.querySelector('.linking');
-        var linkToMenu = document.getElementById('link_to_menu');
-        function onScanSuccess(decodedText, decodedResult) {
-            // Handle on success condition with the decoded text or result.
-            linking.style.display = "block";
-            linkToMenu.href = decodedText;
-            html5QrcodeScanner.clear();
-        }
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", { fps: 10, qrbox: 250 });
-        html5QrcodeScanner.render(onScanSuccess);
+<div id="reader"></div>
+<div class="linking text-center mt-5">
+    <a href="http://localhost:8000/categories/2/1" id="link_to_menu" class="btn btn-success">გადადით ლინკზე</a>
+</div>
+<script src="/html5-qrcode.min.js"></script>
+<script>
+    var linking = document.querySelector('.linking');
+    var linkToMenu = document.getElementById('link_to_menu');
 
-        html5QrcodeScanner.render(onScanSuccess, onScanError);
-    </script>
+    function onScanSuccess(decodedText, decodedResult) {
+        // Handle on success condition with the decoded text or result.
+        linking.style.display = "block";
+        linkToMenu.href = decodedText;
+        html5QrcodeScanner.clear();
+    }
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader", {
+            fps: 10,
+            qrbox: 250
+        });
+    html5QrcodeScanner.render(onScanSuccess);
+  
+    html5QrcodeScanner.render(onScanSuccess, onScanError);
+</script>
 @endsection
