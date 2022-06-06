674a805a5d217b2bc986365b0982dbf89c8a5ae9
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-07 03:50:33 +0400

    added styles on qr code page

--- resources/views/modules/frontend/QR/qr.blade.php
+++ resources/views/modules/frontend/QR/qr.blade.php
@@ -1,9 +1,15 @@
 @extends('parent')
 @section('content')
-<div id="reader"></div>
-<div class="linking text-center mt-5">
-    <a href="http://localhost:8000/categories/2/1" id="link_to_menu" class="btn btn-success">გადადით ლინკზე</a>
-</div>
+<main class="d-flex justify-content-center align-items-center app-screen-height" style="background:#FCFAEB;">
+    <div class="container-fluid pb-5">
+        <div class="container">
+            <div id="reader"></div>
+            <div class="linking text-center mt-5">
+                <a href="http://localhost:8000/categories/2/1" id="link_to_menu" class="btn btn-success">გადადით ლინკზე</a>
+            </div>
+        </div>
+    </div>
+</main>
 <script src="/html5-qrcode.min.js"></script>
 <script>
     var linking = document.querySelector('.linking');
@@ -21,7 +27,7 @@
             qrbox: 250
         });
     html5QrcodeScanner.render(onScanSuccess);
-  //onscanError function should have defined
+    //onscanError function should have defined
     html5QrcodeScanner.render(onScanSuccess, onScanError);
 </script>
 @endsection
