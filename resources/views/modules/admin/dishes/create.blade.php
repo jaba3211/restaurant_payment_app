ed7c59d3c260e1eb76bf82022f73fd9de69bbc21
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-12 13:53:44 +0400

    added back button on add dish page, with no href

--- resources/views/modules/admin/dishes/create.blade.php
+++ resources/views/modules/admin/dishes/create.blade.php
@@ -1,15 +1,18 @@
 @extends('parent')
 @section('content')
-    <main class="d-flex justify-content-center align-items-center app-screen-height" style="background:#FCFAEB;">
-        <div class="container-fluid">
-            <div class="container">
-                <div class="d-flex align-items-center flex-column main-comps-container">
-                    <h2 class="login_form_heading text-center my-3 text-warning">
-                        Add Dish To The Menu
-                    </h2>
-                    @include('modules.admin.dishes.forms')
+<main class="d-flex justify-content-center align-items-center app-screen-height" style="background:#FCFAEB;">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="d-flex align-items-center flex-column main-comps-container">
+                <div class="text-start w-100">
+                    <a href="#" class="btn btn-warning mb-3">Back</a>
                 </div>
+                <h2 class="login_form_heading text-center my-3 text-warning">
+                    Add Dish To The Menu
+                </h2>
+                @include('modules.admin.dishes.forms')
             </div>
         </div>
-    </main>
+    </div>
+</main>
 @endsection
