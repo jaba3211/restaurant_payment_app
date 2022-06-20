acc0d0b3c05b3fb4c0485fa7769a53b70d8e53ac
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-20 22:45:34 +0400

    fixed restaurants list delete popup window

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
 
+    var mycards = document.querySelectorAll('.mycard');
+    var mybody = document.body;
+    // var delete_modal;
+    mycards.forEach(e => {
+
+        var deleteLink = e.querySelector('.delete');
+        var delete_modal = e.querySelector('.delete_modal');
+        var close_btn = e.querySelector('.close_btn');
+        var cancel_btn = e.querySelector('.cancel_btn');
+
+        function appear_modal() {
+            mybody.classList.add('body_wrapper');
+            delete_modal.classList.add('my_show');
+        }
+        deleteLink.onclick = appear_modal;
+
+        function disappear_modal() {
+            mybody.classList.remove('body_wrapper');
+
+            delete_modal.classList.remove('my_show');
+        }
+        close_btn.onclick = disappear_modal;
+        cancel_btn.onclick = disappear_modal;
+
+    });
+   
--- resources/views/modules/admin/restaurants/list.blade.php
+++ resources/views/modules/admin/restaurants/list.blade.php
@@ -1,20 +1,22 @@
 @foreach($list as $row)
-<div class="card mb-2">
+<div class="card mb-2 mycard">
     <h5 class="card-header">{{ $row->name }}</h5>
     <h6 class="card-header text-info bg-transparent">ID: {{ $row->id }}</h6>
     <div class="card-body">
         <!-- Button trigger modal -->
-        <button class="btn btn-danger delete">
+        <!-- <button class="btn btn-danger delete">
             DELETE
-        </button>
-
+        </button> -->
+        <div class="btn btn-danger delete">delete</div>
         <!-- Modal -->
         <div class="delete_modal">
             <div class="modal_top d-flex justify-content-between">
                 <h6 class="modal_heading text-danger">
                     Are you sure you want to delete this item?
                 </h6>
-                <span class="close_btn"><ion-icon name="close"></ion-icon></span>
+                <span class="close_btn">
+                    <ion-icon name="close"></ion-icon>
+                </span>
             </div>
             <div class="modal_description">
                 This item will be deleted immediately. You can't undo this action!
@@ -29,32 +31,7 @@
 
         <a href="{{ route('dishes', ['restaurant_id' => $row['id']]) }}" class="btn btn-primary admin_change_btn" style="background:#f0bb18; border:0;">DISHES</a>
         <a href="{{ route('categories', ['restaurant_id' => $row['id']]) }}" class="btn btn-secondary admin_change_btn" style="background:#73ee73; border:0;">CATEGORIES</a>
+
     </div>
 </div>
 @endforeach
-<script>
-    var deleteLinks = document.querySelectorAll('.delete');
-    // var delete_modal;
-
-    deleteLinks.forEach(element => {
-        element.onclick = function() {
-            // console.log(element);
-            var delete_modal = document.querySelector('.delete_modal');
-            // console.log(delete_modal);
-            document.body.classList.add('body_wrapper');
-            delete_modal.classList.add('show');
-            var close_btn = document.querySelector('.close_btn');
-            var cancel_btn = document.querySelector('.cancel_btn');
-            close_btn.onclick = function() {
-                document.body.classList.remove('body_wrapper');
-
-                delete_modal.classList.remove('show');
-            }
-            cancel_btn.onclick = function() {
-                document.body.classList.remove('body_wrapper');
-
-                delete_modal.classList.remove('show');
-            }
-        }
-    });
-</script>
