64a957b255133b15d8d232955b8d10e1ba52f5c8
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-23 01:07:18 +0400

    changed dishes list

--- resources/views/modules/frontend/dishes/list.blade.php
+++ resources/views/modules/frontend/dishes/list.blade.php
@@ -1,14 +1,15 @@
 @foreach($list as $row)
-    <a href="{{ route('dishes.show', ['dish_id' => $row->id, 'name' => $row->name]) }}" class="text-decoration-none d-block w-100">
-        <div class="dishes d-flex align-items-center w-100 card mb-3 shadow-sm flex-row">
-            <span>
-                <img src="{{ url('/storage/'.$row->image) }}" alt="{{ $row->image }}" class="img-fluid" width="200">
-            </span>
-            <span class="menu_cat_name ms-3 fw-bold">
-                {{ $row->name }}
-                <span class="d-block fw-normal" style="font-size:14px;">{{ $row->short_desc }}</span>
-                <span class="dish_price mb-3 d-block">{{ $row->price }}₾</span>
-            </span>
-        </div>
-    </a>
+<a href="{{ route('dishes.show', ['dish_id' => $row->id, 'name' => $row->name]) }}" class="text-decoration-none d-block w-100">
+    <div class="dishes d-flex align-items-center w-100 card mb-3 shadow-sm flex-row">
+        <span>
+            <img src="{{ url('/storage/'.$row->image) }}" alt="{{ $row->image }}" class="img-fluid" width="200">
+        </span>
+        <span class="menu_cat_name ms-3 fw-bold">
+            {{ $row->name }}
+            <span class="d-block fw-normal" style="font-size:14px;">{{ $row->short_desc }}</span>
+            <span class="dish_price mb-3 d-block">{{ $row->price }}₾</span>
+        </span>
+    </div>
+</a>
+
 @endforeach
