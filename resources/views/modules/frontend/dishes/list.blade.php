a7c165b97054fdba30165401a140c01cf2bfec48
unknown <mariam.markozashvili111@ens.tsu.edu.ge>
2022-06-23 01:45:50 +0400

    added add button on dishes list page

--- resources/views/modules/frontend/dishes/list.blade.php
+++ resources/views/modules/frontend/dishes/list.blade.php
@@ -1,8 +1,10 @@
+
+<form action="#" method="POST" class="w-100">
 @foreach($list as $row)
 <a href="{{ route('dishes.show', ['dish_id' => $row->id, 'name' => $row->name]) }}" class="text-decoration-none d-block w-100">
-    <div class="dishes d-flex align-items-center w-100 card mb-3 shadow-sm flex-row">
+    <div class="dishes d-flex align-items-center w-100 card mb-3 shadow-sm flex-row flex-wrap">
         <span>
-            <img src="{{ url('/storage/'.$row->image) }}" alt="{{ $row->image }}" class="img-fluid" width="200">
+            <img src="{{ url('/storage/'.$row->image) }}" alt="{{ $row->image }}" class="img-fluid" width="150">
         </span>
         <span class="menu_cat_name ms-3 fw-bold">
             {{ $row->name }}
@@ -13,3 +15,7 @@
 </a>
 
 @endforeach
+
+<button type="submit" class="rounded-circle btn btn-danger position-fixed fs-3" style="bottom:20px; right:20px; width:80px; height:80px;">Add</button>
+
+</form>
